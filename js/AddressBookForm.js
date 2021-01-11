7a7a09b64c2b91fa83f7a95a0675e0666356c178
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2021-01-11 07:44:59 +0530

    Added:values stored in local storage

--- js/AddressBookForm.js
+++ js/AddressBookForm.js
@@ -29,3 +29,57 @@ window.addEventListener('DOMContentLoaded', (event) => {
             }
     });    
 }); 
+
+const save = () => {
+    try{
+        let addressBookData = createAddressBookData();
+        createAndUpdateStorage(addressBookData);
+    } catch(exception){
+        return;
+    }
+}
+
+function createAndUpdateStorage(addressBookData) {
+    let addressBookList = JSON.parse(localStorage.getItem("AddressBookList"));
+    if(addressBookList != undefined) {
+        addressBookList.push(addressBookData);
+    } else {
+        addressBookList = [addressBookData];
+    }
+    alert(addressBookList.toString());
+    localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+}
+
+const createAddressBookData = () => {
+    let addressBookData = new AddressBookData();
+    try{
+        addressBookData.name = getInputValueById('#name');
+    } catch(exception){
+        setTextValue('.name-error', exception);
+    }
+    addressBookData.id = createNewId();
+    addressBookData.address = getInputValueById('#address');
+    addressBookData.city = getInputValueById('#city');
+    addressBookData.state = getInputValueById('#state');
+    addressBookData.zip = getInputValueById('#zip');
+    addressBookData.tel = getInputValueById('#tel');
+    alert(addressBookData.toString());
+    return addressBookData;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
+
+const createNewId = () => {
+    let addressBookId = localStorage.getItem("AddressBookId");
+    addressBookId = !addressBookId ? 1 : (parseInt(addressBookId) + 1).toString();
+    localStorage.setItem("AddressBookId", addressBookId);
+    return addressBookId;
+};
